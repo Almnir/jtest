d8b45a6cb5367e2fc65091e602410c86c2b26839
Alm <almnir.mail@gmail.com>
2012-05-31 01:09:04 +0400

    Initial

---
+++ src/jtest.java
@@ -0,0 +1,19 @@
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+
+public class jtest {
+    static Logger logger = Logger.getLogger(jtest.class);
+
+    public static void doIt() {
+        logger.debug("Did it again!");
+    }
+    public static void main(String[] args)
+    {
+        BasicConfigurator.configure();
+        logger.info("Entering application.");
+        doIt();
+        logger.fatal("Unable to proceed!");
+        logger.info("Exiting application.");
+    }
+}
